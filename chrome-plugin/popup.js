7506f8b890021428437a75b3c8a01ce18e3137a9
yxc <2741822345@qq.com>
2024-03-10 01:42:50 +0800

    bookmark

--- chrome-plugin/popup.js
+++ chrome-plugin/popup.js
@@ -1,6 +1,11 @@
 (function () {
   const syncBtn = document.getElementById('syncBtn');
-  syncBtn.addEventListener('click',  function() {
-    console.log('-------')
+
+  syncBtn.addEventListener('click', function () {
+    // 发送消息给background.js
+    chrome.runtime.sendMessage({ data: { type: 'bookmark' } }, function (response) {
+      // 接收来自background.js的响应消息
+      console.log('接收到来自background.js的响应消息:', response);
+    });
   })
 })()
--- chrome-plugin/service-worker.js
+++ chrome-plugin/service-worker.js
@@ -14,24 +14,44 @@ const treeToArrayStack = root => {
 
   return result
 }
-// (function () {
-//   chrome.bookmarks.getTree(data => {
-//     const arr = treeToArrayStack(data[0]);
-//     const list = arr.map(item => {
-//       return {
-//         bId: String(item.id),
-//         parentId: String(item.parentId),
-//         title: item?.title,
-//         dateAdded: String(item.dateAdded),
-//         url: item?.url,
-//       }
-//     });
-//     fetch('http://localhost:7001/api/bookmark', {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json;charset=UTF-8'
-//       },
-//       body: JSON.stringify(list),
-//     });
-//   });
-// })()
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  // 处理接收到的消息
+  console.log('接收到来自popup.js的消息:', request.data);
+  sendResponse({ response: request.data });
+
+  if (request && request.data && request.data.type) {
+    if (request.data.type === 'bookmark') {
+      chrome.bookmarks.getTree(data => {
+        const arr = treeToArrayStack(data[0]);
+        const list = arr.map(item => {
+          return {
+            bId: String(item.id),
+            parentId: String(item.parentId),
+            title: item?.title,
+            dateAdded: String(item.dateAdded),
+            url: item?.url,
+          }
+        });
+        fetch('https://apis.oyxco.com/api/bookmark', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json;charset=UTF-8'
+          },
+          body: JSON.stringify(list),
+        })
+        // .then(response => {
+        //   if (response.ok) {
+        //     return response.json(); // 将响应体转换为JSON
+        //   }
+        //   throw new Error('Network response was not ok.');
+        // }).then(data => {
+        //   // 发送响应消息给popup.js
+        //   sendResponse({ response: data });
+        // }).catch(error => {
+        //   sendResponse({ response: null });
+        // }); // 捕获错误
+      });
+    }
+  }
+});
